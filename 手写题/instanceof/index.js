281a8e53663ea94b48fc2012376f0ff86d00e04d
小鱼不想写代码 <1260314822@qq.com>
2022-02-24 10:57:50 +0800

    instanceof

---
+++ 手写题/instanceof/index.js
@@ -0,0 +1,13 @@
+function myInstanceOf(left,right){
+    //获取对象的原型
+    let proto = Object.getPrototypeOf(left)
+    let prototype = right.prototype
+
+    while(true){
+        if(!proto) return false
+        if(proto === prototype) return true
+        proto = Object.getPrototypeOf(proto)
+    }
+}
+
+console.log(myInstanceOf(2,Number));
