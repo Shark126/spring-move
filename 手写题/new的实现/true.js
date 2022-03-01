2617e0b0609d22650dfecedc062e42dd8a690bf7
小鱼不想写代码 <1260314822@qq.com>
2022-03-01 22:58:55 +0800

    leetcode

--- 手写题/new的实现/true.js
+++ 手写题/new的实现/true.js
@@ -1,21 +1,20 @@
 function myNew(ctor){
-    if( typeof ctor !== 'function'){
-        throw '这个参数不是函数!'
-    }
-    myNew.target = ctor
+ if(typeof ctor !== 'function'){
+     throw '参数不是一个函数'
+ }
+ myNew.target = ctor
+ var newObj = Object.create(ctor.prototype)
+ var arrsObj = [].slice.call(arguments,1)
 
-    var newObj = Object.create(ctor.prototype)
-    var arrs = [].slice.call(arguments,1)
+ var objReturn = ctor.apply(newObj,arrsObj)
 
-    var ctorResult = ctor.apply(newObj,arrs)
+ var isObject = typeof objReturn === 'object' && objReturn !== null
+ var isFunction = typeof objReturn === 'function'
+ if(isObject || isFunction){
+     return objReturn
+ }
 
-    var isObject = typeof ctorResult === 'object' && typeof ctorResult !== null
-    var isFunction = typeof ctorResult === 'function' 
-    if(isObject || isFunction){
-        return ctorResult
-    }
-
-    return newObj
+ return newObj
 }
 
 function MyBaby(name,age){
@@ -25,3 +24,4 @@ function MyBaby(name,age){
 
 var newObj = myNew(MyBaby,'王敏',22)
 console.log(newObj);
+
---
+++ 手写题/关于0.1+0.2 !== 0.3/index.js
@@ -0,0 +1,7 @@
+let n1 = 0.1
+let n2 = 0.2
+console.log(n1+n2);
+
+//可以用toFixed()
+
+console.log((n1+n2).toFixed(2),'你好你好');
