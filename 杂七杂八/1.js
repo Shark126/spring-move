7d09c0a1eb7165ac28a68bc758d790cb34c550c2
“fish-yu” <1260314822@qq.com>
2022-03-27 22:39:13 +0800

    隐藏文字

--- 杂七杂八/1.js
+++ 杂七杂八/1.js
@@ -1,9 +1,10 @@
-let obj = {
-    num1: 117
-}
-let res = obj;
-console.log(res);
-obj.child = obj = { num2: 935 };
-console.log(obj, '----', obj.child);
+// let obj = {
+//     num1: 117
+// }
+// let res = obj;
+// console.log(res);
+// obj.child = obj = { num2: 935 };
+// console.log(obj, '----', obj.child);
 
-console.log('nihao');
+// console.log('nihao');
+a = 1;var a;console.log(a);
