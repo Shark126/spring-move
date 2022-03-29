f23696a1856bcc678488fb1aae084d15686e4ed7
“fish-yu” <1260314822@qq.com>
2022-03-29 23:26:40 +0800

    立即执行函数

--- 杂七杂八/1.js
+++ 杂七杂八/1.js
@@ -1,10 +1,10 @@
-// let obj = {
-//     num1: 117
-// }
-// let res = obj;
-// console.log(res);
-// obj.child = obj = { num2: 935 };
-// console.log(obj, '----', obj.child);
-
-// console.log('nihao');
-a = 1;var a;console.log(a);
+function a(xx){
+    this.x = xx 
+    return this
+}
+var y = a(6)
+console.log(y);
+var x = a(5)
+console.log(x);
+// console.log(z.x);
+// console.log(y.x);
