f1ec1c55429d1e0557288108be749d2870b76212
“fish-yu” <1260314822@qq.com>
2022-04-02 22:44:13 +0800

    pig

---
+++ 算法学习/五大排序/插入排序/index.js
@@ -0,0 +1,22 @@
+// 插入排序
+const insertionSort = array =>{
+    const len = array.length
+    if(len < 1 )return
+
+    let preIndex,current;
+    for(let i = 1 ; i < len ; i++ ){
+        preIndex = i - 1 //待比较元素的下标
+        current = array[i] //当前元素
+        while( preIndex >= 0 && array[preIndex] > current){
+            // 前置条件之一：待比较元素比当前元素大
+            array[preIndex + 1] = array[preIndex] //将待比较元素后移一位
+            preIndex-- //游标向前移一位
+        }
+        if(preIndex + 1 != i){
+            //避免同一个元素赋给自身
+            array[preIndex+1] = current //将当前元素插入预留空位
+            console.log('array',array);
+        }
+    }
+    return array
+}
