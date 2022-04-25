6291ef2e1eeff64de831c7deb411aeccb49ae7c1
“fish-yu” <1260314822@qq.com>
2022-04-25 22:16:25 +0800

    git

--- 杂七杂八/2.js
+++ 杂七杂八/2.js
@@ -1,6 +1,18 @@
-let a = 1
-let b = new Number(1)
+var abc = function(){
+    console.log('123');
+}
 
-console.log(b);
-console.log(a === b);
-console.log(a == b);
+var nn = [1,2,3]
+var bb = '年后'
+var aa = {
+    a: 1,
+    b: 2
+}
+
+console.log(abc.toString);
+console.log(nn.toString);
+console.log(aa.toString);
+console.log(Object.prototype.toString.call(abc));
+console.log(Object.prototype.toString.call(nn));
+
+console.log(typeof NaN);
