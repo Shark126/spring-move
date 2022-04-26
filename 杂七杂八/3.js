8370f98c722df4a7047f7a4332026caa41a2fac4
“fish-yu” <1260314822@qq.com>
2022-04-26 23:24:14 +0800

    git

--- 杂七杂八/3.js
+++ 杂七杂八/3.js
@@ -1,1 +1,35 @@
 // 摸鱼项目目录
+Promise.resolve(11).then(function(value){
+    console.log(value);
+})
+
+
+function testPromise(ready){
+    return new Promise(function(resolve,reject){
+        if(ready){
+            resolve('hello  world')
+        }else{
+            reject('No thanks')
+        }
+    })
+}
+
+testPromise(false).then(function(value){
+    console.log(value);
+},function(error){
+    console.log(error);
+})
+
+
+const test2 = new Promise(function(resolve,reject){
+console.log(1);
+  resolve('success')
+  console.log(2);
+})
+
+test2.then(function(value){
+    console.log(value);
+})
+
+
+ 
---
+++ 杂七杂八/set.js
@@ -0,0 +1,84 @@
+
+console.log('script start');
+
+setTimeout(function () {
+  console.log('setTimeout');
+}, 0);
+
+Promise.resolve()
+  .then(function () {
+    console.log('promise1');
+  })
+  .then(function () {
+    console.log('promise2');
+  });
+
+console.log('script end');
+
+//script start
+//script end
+//promise1
+//promise2
+//setTimeout
+
+
+async function async1() {
+    console.log('async1 start')
+    await async2()
+    console.log('async1 end')
+  }
+
+
+  async function async2() {
+    console.log('async2')
+  }
+
+  console.log('script start')
+
+  setTimeout(function () {
+    console.log('setTimeout')
+  }, 0)
+
+  requestAnimationFrame(function () {
+    console.log('requestAnimationFrame')
+  })
+
+  async1();
+
+  new Promise(function (resolve) {
+    console.log('promise1')
+    resolve();
+  }).then(function () {
+    console.log('promise2')
+  })
+
+// script start 、 async1 start 、 async2 、 promise1 、 async1 end 、 promise2 、 setTimeout 、requestAnimationFrame
+
+
+async function f1() {
+    return new Promise((resolve) => {
+      console.log(4)
+      resolve()
+    }).then(() => {
+      console.log(5)
+    })
+  }
+
+
+  async function run() {
+    console.log(1)
+    new Promise((resolve) => {
+      console.log(2)
+      resolve()
+    }).then(() => {
+      console.log(3)
+    })
+    await f1()
+    // 阻塞后面的代码
+    setTimeout(() => {
+      console.log(6)
+    }, 0)
+    console.log(7)
+  }
+
+  run()
