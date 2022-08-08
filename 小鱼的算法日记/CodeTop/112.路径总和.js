3986c35565efe2617085b85f78e6acbf0780914c
“fish-yu” <1260314822@qq.com>
2022-08-09 00:30:21 +0800

    112 push

---
+++ 小鱼的算法日记/CodeTop/112.路径总和.js
@@ -0,0 +1,12 @@
+var hasPathSum = function(root, targetSum) {
+    const traversal = (node,count) =>{
+        if(!node.right && !node.left && count === 0) return true
+        if(!node.right && !node.left) return false
+        if(node.right && traversal(node.right,count - node.right.val)) return true
+        if(node.left && traversal(node.left,count - node.left.val)) return true
+        return false
+    }
+
+    if(!root) return false
+    traversal(root,targetSum - root.val)
+};
