3021cd8f8875036e0209aa78a66948fec8920373
MacBook <aa@aadeMacBook-Air.local>
2023-02-12 11:37:52 +0800

    feat: review

--- 春招日记/手写训练/JS基础/手写instanceof/index.js
+++ 春招日记/手写训练/JS基础/手写instanceof/index.js
@@ -13,6 +13,18 @@ function myInstanceOf(left,rigth){
     }
 }
 
+function testInstanceOf(left,right){
+    let proto = Object.getPrototypeOf(left)
+    let prototype = right.prototype
+
+    while(true){
+        if(!proto) return false
+        if(proto === prototype) return true
+
+        proto = Object.getPrototypeOf(proto)
+    }
+}
+
 console.log(myInstanceOf('2',String));
 
 
